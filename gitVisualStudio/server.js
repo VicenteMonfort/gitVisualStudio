6930ad74cae1d3f3ec6710f3c4c506d60e22c8ba
VicenteMonfort <vmonfort@florida-uni.es>
2020-10-19 17:38:55 +0200

    Desarrollando Func1

--- gitVisualStudio/server.js
+++ gitVisualStudio/server.js
@@ -5,4 +5,5 @@ var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello New World\n');
+    res.end('HelloFunc1\n');
 }).listen(port);
